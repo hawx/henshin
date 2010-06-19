54874bb7df250406f54b196acb2022c2fb24378f
Joshua Hawxwell <m@hawx.me>
2010-06-19 16:35:13 +0100

    Added highlight plugin

---
+++ lib/henshin/plugins/highlight.rb
@@ -0,0 +1,28 @@
+require 'simplabs/highlight'
+
+
+class HighlightPlugin < Henshin::Generator
+  
+  #puts Simplabs::Highlight.highlight(:ruby, 'class Test; end')
+  
+  def initialize
+    @extensions = {:input => ['markdown'],
+                   :output => ''}
+    @config = {}
+  end
+  
+  def configure( override )
+    @config.merge!(override) if override
+  end
+  
+  def generate( content )
+    content =~ /(\$highlight)\s+(.+)((\n.*)+)(\$end)/
+    if $1
+      lang = $2.to_sym
+      code = $3[1..-1] # removes first new line
+      '<pre><code>' + Simplabs::Highlight.highlight(lang, code) + '</code></pre>'
+    end
+  end
+  
+  Henshin.register! self
+end
