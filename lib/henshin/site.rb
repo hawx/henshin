40f98cde6970a36ce9dc2c394ff72984bff3c3d2
Joshua Hawxwell <m@hawx.me>
2011-02-13 16:00:01 +0000

    Updated for recent changes.

--- lib/henshin/site.rb
+++ lib/henshin/site.rb
@@ -1,75 +1,61 @@
 require_relative 'base'
 require_relative 'file/page'
 
-%w(coffeescript erb haml liquid maruku redcloth sass).each do |l|
-  require_relative "filter/#{l}"
-end
+#%w(coffeescript erb haml liquid maruku redcloth sass).each do |l|
+#  require_relative "filter/#{l}"
+#end
+
+require_relative 'filter/basic'
 
 module Henshin
   class Site < Base
+  
+    include BasicRender
+        
+    ## Filters
     
-    include_filters ErbFilter, HamlFilter, MarukuFilter,
-                    RedClothFilter, LiquidFilter, :using => Henshin::Page
+    filter 'layouts/*.*', Layout, :internal
+    filter '**/*.(liquid|md|mkd|markdown|erb|haml|textile)', Page
     
-    include_filters CoffeeScriptFilter, SassFilter, ScssFilter
+    ## Renders
+    
+    render '**/:title.liquid' do
+      set :output, 'html'
+      set :title, keys[:title]
+    end
+    
+    render '**/:title.(md|mkd|markdown)' do
+      set :output, 'html'
+      set :title, keys[:title]
+    end
+    
+    render '**/*.sass' do
+      set :output, 'css'
+      set :layout, false # would be better as +set :layout, false+
+    end
+    
+    render '**/*.scss' do
+      set :output, 'css'
+      set :layout, false
+    end
+    
+    render '**/*.coffee' do
+      set :output, 'js'
+      set :layout, false
+    end
+    
+    ## Others
     
     set :layout_paths, ['layouts/*.*', '**/layouts/*.*']
     
     ignore '_site/**', '**/_site/**'
     ignore '*.yml', '**/*.yml'
     
-    
     after_each :write do |file|
       if file.can_write?
         puts "  #{'->'.green} #{file.write_path.to_s.grey}"
       end
     end
-    
-    
-    class MarukuEngine
-      def make(content, data)
-        Maruku.new(content).to_html
-      end
-    end
-    
-    class LiquidEngine
-      def make(content, data)
-        
-      end
-    end
-    
-    class SassEngine
-      def make(content, data)
-        engine = Sass::Engine.new(content, :syntax => :sass)
-        engine.render
-      rescue NameError
-        require 'sass'
-        retry
-      end
-    end
-    
-    render '**/:title.liquid' do
-      set :output, 'html'
-      set :title, keys[:title]
-      
-      apply LiquidEngine
-    end
-    
-    render '**/:title.markdown' do
-      set :output, 'html'
-      set :title, keys[:title]
-      
-      apply MarukuEngine
-    end
-    
-    render '**/*.sass' do
-      set :output, 'css'
-      set :no_layout, true # would be better as +set :layout, false+
-      
-      apply SassEngine
-    end
-    
-    filter 'layouts/*.*', Layout, :internal
     
   end
 end
