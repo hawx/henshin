cbc15eee2673af2998f80925f1947bcd2465fd74
Joshua Hawxwell <m@hawx.me>
2011-02-13 15:57:37 +0000

    Created module to allow definition of renders.

---
+++ lib/henshin/definitions.rb
@@ -0,0 +1,45 @@
+module Henshin
+
+  # Include this in a module so that it can be included in a subclass of Henshin::Base
+  # to populate with predefined #render calls. Define them in exactly the same way.
+  #
+  # @example
+  #
+  #   module LiquidRender
+  #     include RenderDefinition
+  #
+  #     render '**/:title.liquid' do
+  #       apply LiquidEngine
+  #       set :title, keys[:title]
+  #     end
+  #   end
+  #
+  module RenderDefinition
+  
+    def self.included(mod)
+      mod.extend(ClassMethods)
+      mod.instance_variable_set("@_render", [])
+    end
+    
+    module ClassMethods
+      def render(path, &block)
+        @_render << [path, block]
+      end
+      
+      def included(klass)
+        # set the renders
+        @_render.each do |(path, block)|
+          klass.render(path, &block)
+        end
+      end
+    end
+  
+  end
+  
+  
+  # Include this module to define an engine???
+  module EngineDefinition
+  
+  end
+  
+end
