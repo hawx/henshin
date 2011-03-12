c7d1135519a8febe9c16e851f21cc556d1be7b94
Joshua Hawxwell <m@hawx.me>
2011-03-12 18:48:55 +0000

    Add highlight class to pres with code.

--- lib/henshin/engine/highlight.rb
+++ lib/henshin/engine/highlight.rb
@@ -20,7 +20,7 @@ module Henshin
       if $1
         lang = $2.to_sym
         code = $3[1..-1]
-        insert = "<pre><code>" + Simplabs::Highlight.highlight(lang, code) + "</code></pre>"
+        insert = "<pre class=\"highlight\"><code>" + Simplabs::Highlight.highlight(lang, code) + "</code></pre>"
         content.gsub(/(\$highlight.*\$end)/m, insert)
       else
         content
