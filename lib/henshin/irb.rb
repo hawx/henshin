74819a3fcaf648a6ea831f06786c4aa2bc2c250a
Joshua Hawxwell <m@hawx.me>
2011-03-31 17:25:32 +0100

    Removed henshin/irb as it had little use

--- lib/henshin/irb.rb
+++
@@ -1,9 +0,0 @@
-# IRB Helper file to be loaded during IRB debugging sessions.
-
-source = Pathname.new("./examples/blog")
-dest = source + Henshin::DEFAULTS['dest_suffix']
-
-@site = Henshin::Blog.new({
-  'source' => source,
-  'dest' => dest
-})
