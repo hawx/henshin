5279794c3dad5370f2ee9832358c3c8d989dbdad
Joshua Hawxwell <m@hawx.me>
2011-04-16 17:00:38 +0100

    Added #all_of matcher for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,28 @@ def mock_file(file, content="")
   file.path.stub!(:read).and_return(content)
   file
 end
+
+
+# Not the best example, but
+#
+#   it "is an array of Hashes" do
+#     all_of(subject).should be_kind_of Hash
+#   end
+#
+class AllOf
+  def initialize(items)
+    @items = items
+  end
+  
+  def should(*args)
+    @items.all? {|i| i.should(*args)}
+  end
+  
+  def should_not(*args)
+    @items.all? {|i| i.should_not(*args)}
+  end
+end
+
+def all_of(items)
+  AllOf.new(items)
+end
