ba932d933b40d78883b668d577c09daab398e9eb
Joshua Hawxwell <m@hawx.me>
2011-05-28 13:22:58 +0100

    Post specs now better reflect the class being tested

--- spec/henshin/file/post_spec.rb
+++ spec/henshin/file/post_spec.rb
@@ -2,13 +2,31 @@ require 'spec_helper'
 
 describe Henshin::Post do
 
+  let(:source) { Pathname.new(File.dirname(__FILE__)) + '..' + 'test_site' }
+  let(:dest)   { source + '_site' }
+  let(:config) { {'source' => source, 'dest' => dest} }
+  let(:site)   { Henshin::Blog.new(config) }
+    
+  let(:first_post) { 
+    mock_file Henshin::Post.new(source + 'alphabetically-first.md', site) {
+      set :date, Time.new(2012, 12, 21, 6, 45)
+    }
+  }
+  
   subject {
-    mock_file Henshin::Post.new('a-post.md', nil), "---\n
-title: A Post\n
-date: 2012-12-21 06:45\n
----\n
-\n
-Here is a post.\n" }
+    mock_file Henshin::Post.new(source + 'the-subject.md', site) {
+      set :date, Time.new(2012, 12, 21, 6, 45)
+      set :title, "The Subject"
+    }
+  }
+    
+  let(:last_post) {
+    mock_file Henshin::Post.new(source + 'time-wise-last.md', site) { 
+      set :date, Time.new(2012, 12, 21, 6, 46) 
+    }
+  }
+  
+  before { site.files += [subject, first_post, last_post] }
 
   describe "#date" do
     it "returns the date the post was written on" do
@@ -18,19 +36,25 @@ Here is a post.\n" }
   
   describe "#url" do
     it "returns a url with the date in it" do
-      subject.url.should == "/2012/12/21/a-post"
+      subject.url.should == "/2012/12/21/the-subject"
     end
   end
   
   describe "#title" do
     it "gets the title from the yaml" do
-      subject.title.should == "A Post"
+      subject.title.should == "The Subject"
     end
   end
   
   describe "#permalink" do
     it "returns the permalink" do
-      subject.permalink == "/2012/12/21/a-post/index.html"
+      subject.permalink.should == "/2012/12/21/the-subject/index.html"
+    end
+  end
+  
+  describe "#write_path" do
+    it "returns the permalink as a relative pathname" do
+      subject.write_path.to_s.should == '2012/12/21/the-subject/index.html'
     end
   end
   
@@ -43,37 +67,25 @@ Here is a post.\n" }
   end
   
   describe "#<=>" do
-    let(:a) {
-      mock_file Henshin::Post.new('a', nil) { set :date, Time.new(2012, 12, 21, 6, 44) }, ''
-    }
-      
-    let(:s) { mock_file Henshin::Post.new('what-post.md', nil), "---\n
-title: Two Posts\n
-date: 2012-12-21 06:45\n
----\n
-\n
-Hey.\n"}
-      
-    # subject
-    let(:b) {
-      mock_file Henshin::Post.new('b', nil) { set :date, Time.new(2012, 12, 21, 6, 46) }, ''
-    }
-  
     it "compares by date" do
-      (a <=> b).should == -1
+      (subject <=> last_post).should == -1
     end
     
     it "compares by permalink if dates are equal" do
-      (s <=> subject).should == 1
+      (subject <=> first_post).should == 1
     end
   end
   
-  describe "#next" do
-    it "returns the post with the next date after this"
+  describe "#next" do 
+    it "returns the post with the next date after this" do
+      subject.next.should == last_post
+    end
   end
   
   describe "#previous" do
-    it "returns the post before this"
+    it "returns the post before this" do
+      subject.previous.should == first_post
+    end
   end
 
 end
