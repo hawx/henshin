d5a8d4aa757dbf838a0288f745cddbcc13112112
Joshua Hawxwell <m@hawx.me>
2011-06-24 10:51:47 +0100

    fixed highlighting in slim templates

--- lib/henshin/engine/slim.rb
+++ lib/henshin/engine/slim.rb
@@ -12,12 +12,20 @@ class Henshin::Engine
       t.render(MagicHash.new(data), {})
     end
     
+    # This uses a private API so may break in the future!
     class HighlightEngine < ::Slim::EmbeddedEngine
       def on_slim_embedded(engine, *body)
-        lines = collect_text(body).split("\n")
-        lang = lines[0][1..-1]
-        code = lines[1..-1].join("\n")
-        [:static, Support::Highlighter.highlight(code, lang)]
+        lines = body.map { |i|
+          collect_text(i).split("\n")
+        }.flatten
+        
+        if lines.empty?
+          [:static, ""]
+        else
+          lang = lines[0][1..-1]
+          code = lines[1..-1].join("\n")
+          [:static, Support::Highlighter.highlight(code, lang)]
+        end
       end
     end
     
