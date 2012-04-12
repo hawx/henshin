d0645b4993e2142a2a16242cacc89bab044d5e32
Joshua Hawxwell <m@hawx.me>
2012-04-12 19:33:32 +0100

    Added henshin.build so sites can be built

--- lib/henshin.rb
+++ lib/henshin.rb
@@ -7,5 +7,18 @@ require 'clive/output'
 end
 
 module Henshin
+  extend self
 
+  # @param root [Pathname]
+  # @return [Site, nil]
+  def build(root, opts={})
+    if (root + 'config.yml').exist?
+      s = Site.new(root)
+      s.build
+      s
+    else
+      UI.fail "No henshin site found, create one use `henshin new`."
+      nil
+    end
+  end
 end
