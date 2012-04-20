35a4eca938e58864ef0bec9a651404d1d7afcb89
Joshua Hawxwell <m@hawx.me>
2012-04-20 15:50:40 +0100

    Moved tag and tags into files/ folder

---
+++ lib/henshin/files/tags.rb
@@ -0,0 +1,51 @@
+module Henshin
+
+  class Tags
+
+    include FileInterface
+
+    def self.create(site, posts)
+      names = posts.map(&:tags).flatten.uniq
+      tags = names.map {|name| Tag.new(name, posts, site) }
+      new(site, tags)
+    end
+
+    def initialize(site, tags)
+      @site = site
+      @tags = tags
+    end
+
+    def text
+      template = @site.template!('tag_index')
+
+      if template
+        SlimEngine.render template.text, @site.data
+      end
+    end
+
+    def files
+      @tags.sort
+    end
+
+    def data
+      files.map(&:data)
+    end
+
+    def permalink
+      '/tag/index.html'
+    end
+
+    def url
+      '/tag/'
+    end
+
+    def write(*args)
+      super(*args) if @site.template!('tag_page')
+    end
+
+    def inspect
+      "#<Tags [#{@tags.map(&:name).join(', ')}]>"
+    end
+
+  end
+end
