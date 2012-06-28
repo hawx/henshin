0ee3b89be96221c1cbd67aaccf5c36f56ef7ef0e
Joshua Hawxwell <m@hawx.me>
2012-06-28 12:57:19 +0100

    Extract constant for backtrace limit

--- lib/henshin/error.rb
+++ lib/henshin/error.rb
@@ -2,10 +2,12 @@ module Henshin
 
   class Error
 
+    LIMIT = 3
+
     def self.prettify(msg, err)
       puts "\n#{msg}".red.bold
       puts "  #{err.message}"
-      puts err.backtrace.take(3).map {|l| "    #{l}" }.join("\n")
+      puts err.backtrace.take(LIMIT).map {|l| "    #{l}" }.join("\n")
       exit 1
     end
 
